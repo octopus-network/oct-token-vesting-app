1a82fdcc891433c53719d7aff880f605d1b98b8c
chezhe <chezhe@hey.com>
2022-04-22 12:47:58 +0800

    feat: add Beneficiaries

--- src/components/MultiVestingPage.tsx
+++ src/components/MultiVestingPage.tsx
@@ -1,35 +1,36 @@
-import React from 'react';
+import React from 'react'
 
-import {
-  GridItem,
-  Grid,
-  Box,
-  useMediaQuery
-} from '@chakra-ui/react';
+import { GridItem, Grid, Box, useMediaQuery } from '@chakra-ui/react'
 
-import Overview from 'components/Overview';
-import Panel from 'components/MultiVestingPanel';
-import Chart from 'components/Chart';
+import Overview from 'components/Overview'
+import Panel from 'components/MultiVestingPanel'
+import Chart from 'components/Chart'
+import IssueBenefitPanel from './IssueBenefitPanel'
 
 type MultiVestingPageProps = {
-  multiVestingContract: any;
+  multiVestingContract: any
 }
 
-export const MultiVestingPage: React.FC<MultiVestingPageProps> = ({ multiVestingContract }) => {
-  const [isDesktop] = useMediaQuery('(min-width: 62em)');
+export const MultiVestingPage: React.FC<MultiVestingPageProps> = ({
+  multiVestingContract,
+}) => {
+  const [isDesktop] = useMediaQuery('(min-width: 62em)')
   return (
-    <Grid templateColumns={isDesktop ? 'repeat(9, 1fr)' : 'repeat(3, 1fr)'} gap="16">
-      {
-        isDesktop &&
+    <Grid
+      templateColumns={isDesktop ? 'repeat(9, 1fr)' : 'repeat(3, 1fr)'}
+      gap="16"
+    >
+      {isDesktop && (
         <GridItem colSpan={6}>
           <Overview />
           <Box mt="12" />
           <Chart />
         </GridItem>
-      }
+      )}
       <GridItem colSpan={3}>
+        <IssueBenefitPanel contract={multiVestingContract} />
         <Panel contract={multiVestingContract} />
       </GridItem>
     </Grid>
-  );
+  )
 }
